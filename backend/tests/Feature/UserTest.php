50c0d33e9a80d70d59310f6a700659d49ee4c06c
Fikriarkani <77955970+Fikriarkani@users.noreply.github.com>
2023-11-13 00:09:15 +0700

    menambahkan test case untuk login dan register

---
+++ backend/tests/Feature/UserTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    use DatabaseMigrations;
+    use RefreshDatabase;
+
+    public function test_create_new_user()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->post('auth/register',[
+            'username' => 'mahasiswa',
+            'full_name' => 'mahasiswaA',
+            'password' => 'mhs123',
+            'email' => 'mhsmhs123@mail.com',
+            'birth_date' => '2002-08-17',
+            'phone_num' => '+6281111111111',
+            'school' => 'SMAN 1 Bandung',
+            'major' => 'Saintek',
+            'grad_date' => '2024'
+        ]);
+        $response->assertOk();
+        /* $response = $this->get('/');
+
+        $response->assertStatus(200); */
+    }
+
+    public function test_login_user()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->post('auth/login',[
+            'password' => 'mhs123',
+            'email' => 'mhsmhs123@mail.com',
+        ]);
+        $response->assertOk();
+    }
+}
